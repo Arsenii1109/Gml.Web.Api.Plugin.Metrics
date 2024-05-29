e877da0a927c73f85c3322bdbbd606b1b3ef0733
Akemiko <tcop29711@gmail.com>
2024-05-29 19:51:45 +0300

    Add user management features to API plugin and update gitignore
    This commit introduces new API endpoints related to user management in the Gml.Web.Api.Plugins.Metrics project. It includes functionality for getting user information, updating ban status, and fetching a list of all users. The gitignore file has also been updated with rules specific to Visual Studio, Mac, Windows and other common IDEs and operating systems.

---
+++ Dto/UserBanUpdateDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Gml.Web.Api.Plugins.Metrics.Dto;
+
+public class UserBanUpdateDto
+{
+    public bool IsBanned { get; set; }
+    public string? User { get; set; }
+}
---
+++ MainClass.cs
@@ -0,0 +1,72 @@
+﻿using Gml.Web.Api.EndpointSDK;
+using Gml.Web.Api.Plugins.Metrics.Dto;
+using GmlCore.Interfaces;
+using GmlCore.Interfaces.User;
+using Microsoft.AspNetCore.Http;
+
+namespace Gml.Web.Api.Plugins.Metrics;
+
+[Path("GET", "/api/v1/plugins/users", true)]
+public class GetUsers : EndpointHelper, IPluginEndpoint
+{
+    public async Task Execute(HttpContext context, IGmlManager gmlManager)
+    {
+        IEnumerable<IUser> users = await gmlManager.Users.GetUsers();
+        
+        await Ok(context, users, "Пользователи");
+    }
+}
+
+[Path("POST", "/api/v1/plugins/users/ban", true)]
+public class BanUser : EndpointHelper, IPluginEndpoint
+{
+    public async Task Execute(HttpContext context, IGmlManager gmlManager)
+    {
+        var body = await ParseDto<UserBanUpdateDto>(context);
+
+        if (body is null || string.IsNullOrEmpty(body.User))
+        {
+            await BadRequest(context, "Неверный формат данных");
+            return;
+        }
+
+        var user = await gmlManager.Users.GetUserByName(body.User);
+        
+        if (user is null)
+        {
+            await BadRequest(context, "Данный пользователь не найден");
+            return;
+        }
+        
+        user.IsBanned = body.IsBanned;
+
+        await gmlManager.Users.UpdateUser(user);
+        
+        await Ok(context, user, "Пользователи");
+    }
+}
+
+[Path("GET", "/api/v1/plugins/users/info", true)]
+public class ProfileInfo : EndpointHelper, IPluginEndpoint
+{
+    public async Task Execute(HttpContext context, IGmlManager gmlManager)
+    {
+        var userName = context.Request.Query["user"];
+        
+        if (string.IsNullOrEmpty(userName))
+        {
+            await BadRequest(context, "Неверный формат данных");
+            return;
+        }
+
+        var user = await gmlManager.Users.GetUserByName(userName);
+        
+        if (user is null)
+        {
+            await BadRequest(context, "Данный пользователь не найден");
+            return;
+        }
+        
+        await Ok(context, user, "Пользователи");
+    }
+}
